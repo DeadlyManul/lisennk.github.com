632eec9fae4a77e4a6ed0dd61a970ee87eb83d36
serewann <serewann@gmail.com>
2016-10-16 22:02:06 +0300

    My skills

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 var moduleImporter = require('sass-module-importer');
 
-
-gulp.task('default', function() {
-    gulp.src('./src/sass/main.scss')
+gulp.task('sass', function() {
+    return gulp.src('./src/sass/main.scss')
         .pipe(sass({ importer: moduleImporter() }))
         .pipe(gulp.dest('public'));
 });
+
+gulp.task('watch', function() {
+    gulp.watch('src/sass/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass']);
